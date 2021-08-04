f1cf61f750211262d6252bae8cfd756e544dfa4f
Ryan Wessel <rwessel@dataminr.com>
2021-08-04 14:57:26 -0400

    Make LoadingOverlay fill screen height

--- react/common/components/LoadingOverlay.tsx
+++ react/common/components/LoadingOverlay.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
-import {ActivityIndicator, StyleSheet, View} from 'react-native';
+import {
+  ActivityIndicator,
+  StyleSheet,
+  View,
+  useWindowDimensions,
+} from 'react-native';
 import {useTheme} from '../style/theme';
 
 const styles = StyleSheet.create({
   overlay: {
-    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
 export const LoadingOverlay = () => {
   const theme = useTheme();
+  const {height} = useWindowDimensions();
 
   return (
-    <View style={[styles.overlay, {backgroundColor: theme.background}]}>
+    <View style={[styles.overlay, {backgroundColor: theme.background, height}]}>
       <ActivityIndicator size="large" color={theme.text} />
     </View>
   );
